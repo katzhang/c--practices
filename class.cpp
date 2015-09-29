bff6dc7f7714004663dcf3d0b5aff5210492da9e
katzhang-ias <arcadychang@gmail.com>
2015-09-28 20:52:25 -0400

    added more class features

--- class.cpp
+++ class.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
-class Date {
-	int d, m, y;
+namespace Chrono {
+	enum class Month { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
 
-public:
-	Date(int dd = 0, int mm = 0, int yy = 0);
+	class Date {
+	public: 
+		class Bad_date {};
+		explicit Date(int dd = {}, Month mm = {}, int yy = {}); // {} means "pick a default"
 
-	void add_year(int n);
-	void add_month(int n);
-	void add_day(int n);
+		// nonmodifying functions for examining the Date
+		int day() const;
+		Month month() const;
+		int year() const;
+
+		string string_rep() const;
+		void char_rep(char s[], int max) const;
+
+		// modifying functions for changing the Date
+		Date& add_year(int n);
+		Date& add_month(int n);
+		Date& add_day(int n);
+	private:
+		bool is_valid();
+		int d, m, y;
+	};
+
+	bool is_date(int d, Month m, int y);
+	bool is_leapyear(int y);
+
+	bool operator==(const Date& a, const Date& b);
+	bool operator!=(const Date& a, const Date& b);
+
+	const Date& default_date();
+
+	ostream& operator<<(ostream& os, const Date& d);
+	istream& operator>>(istream& is, Date& d);
 };
 
-Date::Date(int dd, int mm, int yy) {
-	d = dd ? dd : today.d;
-	m = mm ? mm : today.m;
-	y = yy ? yy : today.y;
+int main() {
+	Date d1 {15}; // ok
+	Date d2 = {15}; // error: = initialization does not do implicit conversions
 }
 
